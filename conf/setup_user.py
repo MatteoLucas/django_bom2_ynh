635047d654c85fe70f6339ba041910e4ce80b7fa
Matteo Lucas <matteo.lucas04@gmail.com>
2025-03-24 19:25:36 +0100

    setup_user

--- conf/setup_user.py
+++ conf/setup_user.py
@@ -1,15 +1,12 @@
-from django.contrib.auth.models import User
-
-
-def setup_project_user(user, sso_data: dict):
+def setup_project_user(user, sso_data):
+    """
+    Initialiser les nouveaux utilisateurs créés via SSOwat.
+    """
     user.email = sso_data.get("email", "") or user.email
     user.first_name = sso_data.get("first_name", "") or user.first_name
     user.last_name = sso_data.get("last_name", "") or user.last_name
 
-    # Seul l'utilisateur admin défini à l'installation est superuser
-    from django.conf import settings
-    admin_username = settings.get_env("ADMIN_USERNAME", None)
-    if user.username == admin_username:
+    if user.username == "mlucas":
         user.is_superuser = True
         user.is_staff = True
     else:
@@ -18,4 +15,3 @@ def setup_project_user(user, sso_data: dict):
 
     user.save()
     return user
-
