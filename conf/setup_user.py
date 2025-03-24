9b0a8709216db8e6403e48aed6ab43b5420280cb
Matteo Lucas <matteo.lucas04@gmail.com>
2025-03-24 11:11:39 +0100

    setup_user

--- conf/setup_user.py
+++ conf/setup_user.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.models import User
-from inventory.permissions import get_or_create_normal_user_group
 
 
 def setup_project_user(user: User, sso_data: dict) -> None:
@@ -9,13 +8,11 @@ def setup_project_user(user: User, sso_data: dict) -> None:
     - Assigne au groupe par défaut
     - Active l'accès à l’admin Django
     """
-    user.email = sso_data.get("email", "") or user.email
-    user.first_name = sso_data.get("first_name", "") or user.first_name
-    user.last_name = sso_data.get("last_name", "") or user.last_name
+    if sso_data:
+        user.email = sso_data.get("email", "") or user.email
+        user.first_name = sso_data.get("first_name", "") or user.first_name
+        user.last_name = sso_data.get("last_name", "") or user.last_name
 
-    # Ajoute au groupe "normal user" si nécessaire
-    pyinventory_user_group = get_or_create_normal_user_group()[0]
-    user.groups.set([pyinventory_user_group])
-
+    # Permet l'accès à l'admin Django
     user.is_staff = True
     user.save()
