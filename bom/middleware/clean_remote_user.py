6a97da5e5bfc0e53bb934f214f34b1e6e01dae48
Matteo Lucas <matteo.lucas04@gmail.com>
2025-03-25 08:09:43 +0100

    clean remote user

--- bom/middleware/clean_remote_user.py
+++ bom/middleware/clean_remote_user.py
@@ -1,9 +1,8 @@
-class clean_remote_user:
+class CleanRemoteUserMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        remote_user = request.META.get('HTTP_REMOTE_USER', '')
-        if ',' in remote_user:
-            request.META['HTTP_REMOTE_USER'] = remote_user.split(',')[0]
+        if request.META.get("REMOTE_USER"):
+            request.META["REMOTE_USER"] = request.META["REMOTE_USER"].split(",")[0]
         return self.get_response(request)
